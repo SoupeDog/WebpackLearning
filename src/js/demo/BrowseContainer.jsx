47d6ddbb3c97c2760a1b1cbbfffb60f85249ae4d
Xavier <xavierpe@qq.com>
2018-08-28 16:28:21 +0800

    存档点

--- src/js/demo/BrowseContainer.jsx
+++ src/js/demo/BrowseContainer.jsx
@@ -15,7 +15,6 @@ import TOCIcon from '@material-ui/icons/toc';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import IconButton from "@material-ui/core/es/IconButton/IconButton";
 import Tooltip from "@material-ui/core/es/Tooltip/Tooltip";
-import Slide from "@material-ui/core/es/Slide/Slide";
 import Hidden from "@material-ui/core/es/Hidden/Hidden";
 
 const generateClassName = createGenerateClassName({productionPrefix: "HyggeWriterComponent"});
@@ -31,11 +30,10 @@ class BrowseContainer extends BaseComponent {
         super(props)
         this.state = {
             callbackTheme: this.StyleHelper.getLightTheme_Blue_Pink(),
-            catalog_Hide: false,
+            catalog_Hide: true,
             bgm_Stop: false,
             article_Catalog_NeedChange: false,
             rightMenu_NeedChange: true,
-            editorMd: null,
             WindowsScrollHelper: new WindowsScrollHelper()
         }
         console.log("constructor----------");
@@ -59,6 +57,9 @@ class BrowseContainer extends BaseComponent {
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         console.log("shouldComponentUpdate----------");
+        if (window.innerWidth < 768) {
+            nextState.catalog_Hide = true;
+        }
         // console.log("nextProps:" + JSON.stringify(nextProps));
         // console.log("nextState:" + JSON.stringify(nextState));
         // console.log("nextContext:" + JSON.stringify(nextContext));
@@ -73,7 +74,7 @@ class BrowseContainer extends BaseComponent {
     }
 
     freshArticle() {
-        let mdObj = MarkdownHelper.formatToHtml({
+        MarkdownHelper.formatToHtml({
             text: this.props.article.content,
             id: "article_Content",
             catalogId: "catLogSource"
@@ -96,70 +97,83 @@ class BrowseContainer extends BaseComponent {
                                     style={{
                                         margin: 0,
                                         border: "0",
+                                        padding: "0",
                                         width: "100%",
                                         height: "80px"
                                     }} src="//music.163.com/outchain/player?type=2&id=571541787&auto=0&height=66">
                             </iframe>
                         </Grid>
                         <Grid id="article" item xs={12} container spacing={0} justify="center">
-                            <Grid item xs={this.state.catalog_Hide ? null : 2}>
-                                <Hidden only={"xs"}>
-                                    <div id="article_Catalog" className="hyggeWriter_Markdown_Catalog" style={{
-                                        width: this.state.article_Catalog_NeedChange ? "16.66%" : "100%",
-                                        position: this.state.article_Catalog_NeedChange ? "fixed" : "static",
-                                        top: "60px",
-                                        height: (window.innerHeight - 60) + "px",
-                                        display: this.state.catalog_Hide ? "none" : "block"
-                                    }} dangerouslySetInnerHTML={{__html: $("#catLogSource").html()}}>
-                                    </div>
-                                </Hidden>
-                            </Grid>
-                            <Grid id="article_Main" item xs={this.state.catalog_Hide ? 12 : 10} container spacing={0}
-                                  justify="center">
-                                <Grid item xs={1}  ></Grid>
-                                <Grid item xs={10} container spacing={0} justify="center">
-                                    <div id="article_Content" className="hyggeWriter_Markdown_Reader"
-                                         style={{width: "100%", minHeight: window.innerHeight}}>
-                                    </div>
-                                </Grid>
-                                <Grid item xs={1} container direction="column" justify="flex-start" alignItems="center"
-                                      style={{backgroundColor: "red"}}>
-                                    <Hidden only={"xs"}>
-                                        <div id="article_RightMenu" style={{
-                                            position: this.state.rightMenu_NeedChange ? "fixed" : "static",
-                                            marginTop: this.state.rightMenu_NeedChange ? "0px" : "400px"
-                                        }}>
-                                            <Grid item xs={12} container direction="column" justify="flex-start">
-                                                <Grid item>
-                                                    <Tooltip title="目录" placement="left">
-                                                        <IconButton variant="outlined" color="secondary"
-                                                                    style={{display: "block", margin: "0px auto"}}
-                                                                    onClick={this.catalogTrigger.bind(this)}>
-                                                            <TOCIcon/>
-                                                        </IconButton>
-                                                    </Tooltip>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Tooltip title="返回顶部" placement="left">
-                                                        <IconButton variant="outlined" color="secondary"
-                                                                    style={{display: "block", margin: "0px auto"}}
-                                                                    onClick={() => {
-                                                                        window.scrollTo(0, 330);
-                                                                    }
-                                                                    }>
-                                                            <ArrowUpwardIcon/>
-                                                        </IconButton>
-                                                    </Tooltip>
-                                                </Grid>
-                                            </Grid>
-                                        </div>
-                                    </Hidden>
-                                </Grid>
-                            </Grid>
+                            {this.renderArticleCatLog(this.state.catalog_Hide)}
+                            {this.renderMain(this.state.catalog_Hide)}
                         </Grid>
                     </Grid>
                 </MuiThemeProvider>
             </JssProvider>
+        );
+    }
+
+    renderArticleCatLog(catalogHide) {
+        if (catalogHide) {
+            return (null);
+        } else {
+            return (
+                <Hidden only="xs">
+                    <Grid item xs={2}>
+                        <div id="article_Catalog" className="hyggeWriter_Markdown_Catalog" style={{
+                            width: this.state.article_Catalog_NeedChange ? "16.66666%" : "100%",
+                            position: this.state.article_Catalog_NeedChange ? "fixed" : "static",
+                            top: "60px",
+                            height: (window.innerHeight - 60) + "px",
+                        }} dangerouslySetInnerHTML={{__html: $("#catLogSource").html()}}>
+                        </div>
+                    </Grid>
+                </Hidden>
+            );
+        }
+    }
+
+    renderMain(catalogHide) {
+        return (
+            <Grid id="article_Main" item xs={12} sm={catalogHide ? 12 : 10} container spacing={0} justify="center">
+                <Grid item xs={1}></Grid>
+                <Grid item xs={10} container spacing={0} justify="center">
+                    <div id="article_Content" className="hyggeWriter_Markdown_Reader">
+                    </div>
+                </Grid>
+                <Grid item xs={1} container spacing={0} direction="column" justify="flex-start" alignItems="center">
+                    <Hidden only="xs">
+                        <div id="article_RightMenu" style={{
+                            position: this.state.rightMenu_NeedChange ? "fixed" : "static",
+                            marginTop: this.state.rightMenu_NeedChange ? "0px" : "400px"
+                        }}>
+                            <Grid item xs={12} container spacing={0} direction="column" justify="flex-start">
+                                <Grid item>
+                                    <Tooltip title="目录" placement="left">
+                                        <IconButton variant="outlined" color="secondary"
+                                                    style={{display: "block", margin: "0px auto"}}
+                                                    onClick={this.catalogTrigger.bind(this)}>
+                                            <TOCIcon/>
+                                        </IconButton>
+                                    </Tooltip>
+                                </Grid>
+                                <Grid item>
+                                    <Tooltip title="返回顶部" placement="left">
+                                        <IconButton variant="outlined" color="secondary"
+                                                    style={{display: "block", margin: "0px auto"}}
+                                                    onClick={() => {
+                                                        window.scrollTo(0, 330);
+                                                    }
+                                                    }>
+                                            <ArrowUpwardIcon/>
+                                        </IconButton>
+                                    </Tooltip>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Hidden>
+                </Grid>
+            </Grid>
         );
     }
 
