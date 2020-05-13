080fd47a0aaa99361c146a522d02e35ca0b17d7a
Xavier <xavierpe@qq.com>
2020-05-13 14:48:08 +0800

    更新模板文件

--- src/js/HelloWord.jsx
+++ src/js/HelloWord.jsx
@@ -5,50 +5,79 @@ class HelloWord extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {hasError: false};
         LogHelper.info({className: "HelloWord", msg: "constructor----------"});
     }
 
-    static getDerivedStateFromProps(nextProps, nextContext) {
+    static getDerivedStateFromProps(props, state) {
         LogHelper.info({className: "HelloWord", msg: "getDerivedStateFromProps----------"});
-        LogHelper.debug({className: "HelloWord", tag: "nextProps", msg: nextProps, isJson: true});
-        LogHelper.debug({className: "HelloWord", tag: "nextContext", msg: nextContext, isJson: true});
+        LogHelper.debug({className: "HelloWord", tag: "props", msg: props, isJson: true});
+        LogHelper.debug({className: "HelloWord", tag: "state", msg: state, isJson: true});
         LogHelper.debug({msg: ""});
-        return nextProps;
+        // 它应返回一个对象来更新 state，如果返回 null 则不更新任何内容
+        return state;
     }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
+    shouldComponentUpdate(nextProps, nextState) {
         LogHelper.info({className: "HelloWord", msg: "shouldComponentUpdate----------"});
         LogHelper.debug({className: "HelloWord", tag: "nextProps", msg: nextProps, isJson: true});
         LogHelper.debug({className: "HelloWord", tag: "nextState", msg: nextState, isJson: true});
-        LogHelper.debug({className: "HelloWord", tag: "nextContext", msg: nextContext, isJson: true});
         LogHelper.debug({msg: ""});
+        // 返回 false 以告知 React 可以跳过更新。请注意，返回 false 并不会阻止子组件在 state 更改时重新渲染。
         return true;
     }
 
     render() {
-        return (
-            <>
-                <h1 className="testStyle1">Hello world - 1.</h1>
-                <h1 className="testStyle2">Hello world - 2.</h1>
-            </>
-        );
+        if (this.state.hasError) {
+            // 你可以渲染任何自定义的降级  UI
+            return <h1>Something went wrong.</h1>;
+        } else {
+            return (
+                <>
+                    <h1 className="testStyle1">Hello world - 1.</h1>
+                    <h1 className="testStyle2">Hello world - 2.</h1>
+                </>
+            );
+        }
     }
 
     componentDidMount() {
         LogHelper.info({className: "HelloWord", msg: "componentDidMount----------"});
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    componentDidUpdate(prevProps, prevState, resultFromGetSnapshotBeforeUpdate) {
         LogHelper.info({className: "HelloWord", msg: "componentDidUpdate----------"});
         LogHelper.debug({className: "HelloWord", tag: "prevProps", msg: prevProps, isJson: true});
         LogHelper.debug({className: "HelloWord", tag: "prevState", msg: prevState, isJson: true});
-        LogHelper.debug({className: "HelloWord", tag: "snapshot", msg: snapshot, isJson: true});
+        LogHelper.debug({
+            className: "HelloWord",
+            tag: "resultFromGetSnapshotBeforeUpdate",
+            msg: resultFromGetSnapshotBeforeUpdate,
+            isJson: true
+        });
         LogHelper.debug({msg: ""});
     }
 
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        LogHelper.info({className: "HelloWord", msg: "getSnapshotBeforeUpdate----------"});
+        LogHelper.debug({className: "HelloWord", tag: "prevProps", msg: prevProps, isJson: true});
+        LogHelper.debug({className: "HelloWord", tag: "prevState", msg: prevState, isJson: true});
+        LogHelper.debug({msg: ""});
+        // 此生命周期的任何返回值将作为参数传递给 componentDidUpdate()。
+        let resultFromGetSnapshotBeforeUpdate = null;
+        return resultFromGetSnapshotBeforeUpdate;
+    }
+
     componentWillUnmount() {
+        // 在组件卸载及销毁之前直接调用。在此方法中执行必要的清理操作
         LogHelper.info({className: "HelloWord", msg: "componentWillUnmount----------"});
+    }
+
+    static getDerivedStateFromError(error) {
+        LogHelper.info({className: "HelloWord", msg: "getDerivedStateFromError----------"});
+        LogHelper.error({className: "HelloWord", tag: "error", msg: error, isJson: true});
+        // 更新 state 使下一次渲染可以显降级 UI
+        return {hasError: true};
     }
 }
 
--- src/js/Template.jsx
+++ src/js/Template.jsx
@@ -1,52 +1,82 @@
 import React from 'react';
-import LogHelper from "../../utils/LogHelper.jsx";
+import LogHelper from "./utils/LogHelper.jsx";
 
-// react 16.x 后生命周期钩子函数发生了变化  请注意版本
+// React 版本 16.13.1
 class Template extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {hasError: false};
         LogHelper.info({className: "Template", msg: "constructor----------"});
     }
 
-    static getDerivedStateFromProps(nextProps, nextContext) {
+    static getDerivedStateFromProps(props, state) {
         LogHelper.info({className: "Template", msg: "getDerivedStateFromProps----------"});
-        LogHelper.debug({className: "Template", tag: "nextProps", msg: nextProps, isJson: true});
-        LogHelper.debug({className: "Template", tag: "nextContext", msg: nextContext, isJson: true});
+        LogHelper.debug({className: "Template", tag: "props", msg: props, isJson: true});
+        LogHelper.debug({className: "Template", tag: "state", msg: state, isJson: true});
         LogHelper.debug({msg: ""});
-        return nextProps;
+        // 它应返回一个对象来更新 state，如果返回 null 则不更新任何内容
+        return state;
     }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
+    shouldComponentUpdate(nextProps, nextState) {
         LogHelper.info({className: "Template", msg: "shouldComponentUpdate----------"});
         LogHelper.debug({className: "Template", tag: "nextProps", msg: nextProps, isJson: true});
         LogHelper.debug({className: "Template", tag: "nextState", msg: nextState, isJson: true});
-        LogHelper.debug({className: "Template", tag: "nextContext", msg: nextContext, isJson: true});
         LogHelper.debug({msg: ""});
+        // 返回 false 以告知 React 可以跳过更新。请注意，返回 false 并不会阻止子组件在 state 更改时重新渲染。
         return true;
     }
 
     render() {
-        return (
-            null
-        );
+        if (this.state.hasError) {
+            // 你可以渲染任何自定义的降级  UI
+            return <h1>Something went wrong.</h1>;
+        } else {
+            return (
+                <>
+                </>
+            );
+        }
     }
 
     componentDidMount() {
         LogHelper.info({className: "Template", msg: "componentDidMount----------"});
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    componentDidUpdate(prevProps, prevState, resultFromGetSnapshotBeforeUpdate) {
         LogHelper.info({className: "Template", msg: "componentDidUpdate----------"});
         LogHelper.debug({className: "Template", tag: "prevProps", msg: prevProps, isJson: true});
         LogHelper.debug({className: "Template", tag: "prevState", msg: prevState, isJson: true});
-        LogHelper.debug({className: "Template", tag: "snapshot", msg: snapshot, isJson: true});
+        LogHelper.debug({
+            className: "Template",
+            tag: "resultFromGetSnapshotBeforeUpdate",
+            msg: resultFromGetSnapshotBeforeUpdate,
+            isJson: true
+        });
         LogHelper.debug({msg: ""});
     }
 
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        LogHelper.info({className: "Template", msg: "getSnapshotBeforeUpdate----------"});
+        LogHelper.debug({className: "Template", tag: "prevProps", msg: prevProps, isJson: true});
+        LogHelper.debug({className: "Template", tag: "prevState", msg: prevState, isJson: true});
+        LogHelper.debug({msg: ""});
+        // 此生命周期的任何返回值将作为参数传递给 componentDidUpdate()。
+        let resultFromGetSnapshotBeforeUpdate = null;
+        return resultFromGetSnapshotBeforeUpdate;
+    }
+
     componentWillUnmount() {
+        // 在组件卸载及销毁之前直接调用。在此方法中执行必要的清理操作
         LogHelper.info({className: "Template", msg: "componentWillUnmount----------"});
+    }
+
+    static getDerivedStateFromError(error) {
+        LogHelper.info({className: "Template", msg: "getDerivedStateFromError----------"});
+        LogHelper.error({className: "Template", tag: "error", msg: error, isJson: true});
+        // 更新 state 使下一次渲染可以显降级 UI
+        return {hasError: true};
     }
 }
 
